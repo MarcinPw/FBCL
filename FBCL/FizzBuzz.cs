5c196bfa375917d03edc6285259ab9a81d1245b5
mrc <marc__in@o2.pl>
2017-01-05 15:52:38 +0100

    Tests

---
+++ FBCL/FizzBuzz.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBCL
+{
+    public class FizzBuzz
+    {
+        /// <summary>
+        /// Check if value is divisible by three
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>boolean</returns>
+        public bool DivideByThree(int num)
+        {
+            return num % 3 == 0;
+        }
+        /// <summary>
+        /// Check if value is divisible by five
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>boolean</returns>
+        public bool DivideByFive(int num)
+        {
+            return num % 5 == 0;
+        }
+        /// <summary>
+        /// Check if value is divisible by three and five
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>boolean</returns>
+        public bool DivideByThreeAndFive(int num)
+        {
+            return (num % 3 == 0 && num % 5 == 0);
+        }
+        /// <summary>
+        /// Main game method
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>0 if number is not divisible by 3 or 5</returns>
+        public string Evaluate(int num)
+        {
+            if (DivideByThreeAndFive(num))
+            {
+                return "FizzBuzz";
+            }
+            else if (DivideByFive(num))
+            {
+                return "Buzz";
+            }
+            else if (DivideByThree(num))
+            {
+                return "Fizz";
+            }
+            else
+                return num.ToString();
+        }
+    }
+}
---
+++ FBCL/FizzBuzzTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBCL
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        FizzBuzz fb = new FizzBuzz();
+
+        [Test]
+        public void TestDivideByThree()
+        {
+            Assert.That(fb.DivideByThree(3));
+        }
+        [Test]
+        public void TestDivideByFive()
+        {
+            Assert.That(fb.DivideByFive(5));
+        }
+        [Test]
+        public void TestDivideByThreeAndFive()
+        {
+            Assert.That(fb.DivideByThreeAndFive(15));
+        }
+
+        [Test]
+        public void TestEvaluate()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var result = fb.Evaluate(i);
+                if (result == "Fizz" || result == "Buzz" || result == "FizzBuzz")
+                { Assert.Pass(); }
+                else
+                    Assert.Fail();
+            }
+        }
+    }
+}
